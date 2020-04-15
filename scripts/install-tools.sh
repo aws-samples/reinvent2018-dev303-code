a1212ade8df999b0f6476af08e596ad0bc837e7c
Christoph Kassen <christoph@kassen.name>
2020-04-15 18:05:05 +0200

    Update install-tools.sh
    Use latest kubectl and aws-iam-authenticator

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -34,12 +34,12 @@ pip install --user --upgrade awscli
 sudo yum install bash-completion -y -q
 
 # Install kubectl
-curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl
+curl -o kubectl curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 # Install Heptio Authenticator
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws
+curl -o aws-iam-authenticator curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator && sudo mv aws-iam-authenticator /usr/local/bin/
 
 # Configure AWS CLI
